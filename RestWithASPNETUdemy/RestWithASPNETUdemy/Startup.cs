445dd5aa080564f1684c842b7e324c9a62498758
Felipe <felipe.pereira.dev@gmail.com>
2020-12-07 19:52:30 -0300

    feat: using GenericRepository in Book Business

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -12,6 +12,7 @@ using RestWithASPNETUdemy.Repository;
 using Serilog;
 using System;
 using System.Collections.Generic;
+using RestWithASPNETUdemy.Repository.Generic;
 
 namespace RestWithASPNETUdemy
 { 
@@ -49,7 +50,7 @@ namespace RestWithASPNETUdemy
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
             services.AddScoped<IBookBusiness, BookBusinessImplementation>();
-            services.AddScoped<IBookRepository, BookRepositoryImplementation>(); 
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>)); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
